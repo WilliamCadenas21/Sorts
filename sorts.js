9d4ee0a14e889e06be18c30264701e81a3f7aceb
William Cadenas <williamcadenas21@gmail.com>
2017-11-21 14:32:33 -0500

    select

--- sorts.js
+++ sorts.js
@@ -6,7 +6,7 @@ function quickSort(array){
     var right = [];
     var pivot = array[0];
 
-    for (var i = 1; i < array.length; i++) {
+    for (let i = 1; i < array.length; i++) {
         if (array[i] < pivot) {
             left.push(array[i]);
         }else{
@@ -19,13 +19,11 @@ function quickSort(array){
 
 function bubbleSort(array){
     console.log("BubbleSort:")
-    var aux;
+    
     for (let i = 0; i < array.length - 1; i++) {
         for (let j = 0; j < array.length - i - 1; j++) {
             if (array[j] > array[j+1]) {
-                aux = array[j];
-                array[j] = array[j+1];
-                array[j+1] = aux;   
+                array = swap(array,j,j+1);
             }
         }  
     }
@@ -33,11 +31,32 @@ function bubbleSort(array){
 }
 
 function selectSort(array) {
-    
+    var minId;
+    for (let i = 0; i < array.length - 1; i++) {
+        minId = i;
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[minId]) {
+                minId=j;
+            }
+            
+        }
+        array = swap(array,i,minId);
+    }
+    return array;
 }
 
+function swap(array,i,j) {
+    var aux = array[i];
+    array[i] = array[j];
+    array[j] = aux;
+    return array;   
+}
 
-var arrayZero = [4,9,2,5,1,6,3,8,10,0];
-console.log(bubbleSort(arrayZero));
-console.log("QuickSort:")
-console.log(quickSort(arrayZero)); 
+var arrayZero = [4,9,2,5,7,1,6,3,8,10,0];
+
+//console.log(bubbleSort(arrayZero));
+
+//console.log("QuickSort:")
+//console.log(quickSort(arrayZero));
+
+console.log(selectSort(arrayZero)); 
